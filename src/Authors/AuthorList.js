622faf66d4dcc3840bdce496973872a41d99a78a
Ruslan Valeev <ruvaleev@gmail.com>
2020-08-05 20:36:38 +0700

    presentational components transferred to function components

--- src/Authors/AuthorList.js
+++ src/Authors/AuthorList.js
@@ -2,14 +2,10 @@ import React from  'react';
 
 import AuthorCard from './AuthorCard';
 
-class AuthorList extends React.Component {
-  render() {
-    return (
-      this.props.children.map((author, i) => (
-        <AuthorCard key={author.id} author={author} />
-      ))
-    );
-  }
-}
+const AuthorList = (props) => (
+  props.children.map((author, i) => (
+    <AuthorCard key={author.id} author={author} />
+  ))
+);
 
 export default AuthorList;
